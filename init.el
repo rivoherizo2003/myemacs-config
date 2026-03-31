c3700563d61822d740653c95ce2a8207d3390e14
codeMDG <42198024+rivoherizo2003@users.noreply.github.com>
2026-03-31 19:03:16 +0300

    Update init.el to support symfony 7.4 php framework

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
-;;; init.el --- Emacs Pro Config for EndeavourOS
+;;; init.el --- Emacs Pro Config for Symfony 7.4 & PHP 8+
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(when (file-exists-p custom-file) (load custom-file))
+
 ;; --- 1. Gestion des paquets (MELPA) ---
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -15,110 +16,97 @@
 
 ;; --- 2. Interface et Performance ---
 (setq inhibit-startup-message t)
-(tool-bar-mode -1)          ; Pas d'icônes inutiles
-(menu-bar-mode -1)          ; Pas de menu en haut
-(scroll-bar-mode -1)        ; Pas de barre de défilement
-(setq display-line-numbers-type 'relative) ; Mode relatif activé
-(global-display-line-numbers-mode t)       ; Affiche les numéros
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+(set-fringe-mode 10)
+(setq-default line-spacing 0.1)
+
 (if (daemonp)
-    (add-hook 'server-after-make-frame-hook
-              (lambda () (set-face-attribute 'default nil :height 110)))
+    (add-hook 'server-after-make-frame-hook (lambda () (set-face-attribute 'default nil :height 110)))
   (set-face-attribute 'default nil :height 110))
 
-(use-package doom-themes
-  :config (load-theme 'doom-one t)) ; Un thème sombre moderne
+(use-package doom-themes :config (load-theme 'doom-one t))
+(use-package doom-modeline :init (doom-modeline-mode 1))
+(use-package all-the-icons :if (display-graphic-p))
+(use-package which-key :init (which-key-mode) :config (setq which-key-idle-delay 0.8))
 
-(use-package doom-modeline
-  :init (doom-modeline-mode 1))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.8)) ; Temps d'attente (en secondes) avant l'affichage
 ;; --- 3. Développement (Core) ---
-(use-package magit)         ; Le meilleur client Git
+(use-package magit)
 (use-package projectile
   :init (projectile-mode +1)
   :bind-keymap ("C-c p" . projectile-command-map))
 
-(use-package company        ; Autocomplétion
-  :config (global-company-mode))
+(use-package company
+  :config (global-company-mode)
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 1))
 
 ;; --- 4. LSP Mode (Le cerveau de l'éditeur) ---
 (use-package lsp-mode
   :init (setq lsp-keymap-prefix "C-c l")
-  :hook ((web-mode . lsp)
+  :hook ((php-mode . lsp-deferred)
+         (web-mode . lsp-deferred)
          (typescript-ts-mode . lsp)
          (js-ts-mode . lsp)
-         (php-mode . lsp)
-         (dart-mode . lsp)
-         (java-mode . lsp)
-         (sh-mode . lsp)
-         (yaml-mode . lsp)
-         (sql-mode . lsp))
-  :commands lsp)
-
-(use-package lsp-ui :commands lsp-ui-mode)
-
-;; --- 5. Configuration des langages spécifiques ---
-
-;; Web (HTML/CSS)
-(use-package web-mode
-  :mode ("\\.html\\'" "\\.css\\'")
+         (yaml-mode . lsp))
+  :commands (lsp lsp-deferred)
   :config
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2))
-
-;; Flutter / Dart
-(use-package dart-mode)
-(use-package lsp-dart
-  :hook (dart-mode . lsp))
-
-;; PHP
-(use-package php-mode)
-
-;; YAML
-(use-package yaml-mode)
-
-;; SQL & PL/pgSQL
-(use-package sql-indent)
-
-;; --- 6. Tree-sitter (Coloration syntaxique moderne) ---
-;; Emacs 29+ utilise tree-sitter nativement pour TS/JS/Bash
-(setq major-mode-remap-alist
-      '((typescript-mode . typescript-ts-mode)
-        (js-mode . js-ts-mode)
-        (bash-mode . bash-ts-mode)))
-
-(provide 'init.el)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
-(setq lsp-idle-delay 0.5)
-(setq lsp-log-io nil) ; Désactive les logs pour gagner en vitesse
-
-(use-package all-the-icons
-  :if (display-graphic-p))
-
-(set-fringe-mode 10)       ; Ajoute une marge à gauche et à droite
-(setq-default line-spacing 0.1) ; Un peu d'air entre les lignes
+  (setq lsp-php-server-any-version t) ; Support PHP 8+ pour Symfony 7
+  (setq lsp-idle-delay 0.5)
+  (setq lsp-log-io nil)
+  (setq read-process-output-max (* 1024 1024)))
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
   :config
-  (setq lsp-ui-doc-enable t)      ; Affiche la doc au survol
-  (setq lsp-ui-doc-position 'at-point)
-  (setq lsp-ui-sideline-enable t) ; Affiche les erreurs en bout de ligne
-  (setq lsp-ui-imenu-enable t))
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-position 'at-point
+        lsp-ui-sideline-enable t
+        lsp-ui-imenu-enable t))
+
+;; --- 5. Configuration spécifique SYMFONY & PHP ---
+
+;; PHP Mode avec support des Attributes (PHP 8)
+(use-package php-mode
+  :mode "\\.php\\'"
+  :config
+  (setq php-mode-coding-style 'psr2))
+
+;; Symfony Mode (Navigation & Helper)
+(use-package symfony-mode
+  :ensure t
+  :hook (php-mode . symfony-mode))
+
+;; Twig & Web development
+(use-package web-mode
+  :mode ("\\.html\\.twig\\'" "\\.html\\'" "\\.css\\'")
+  :config
+  (setq web-mode-engines-alist '(("twig" . "\\.html\\.twig\\'")))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+;; Composer integration
+(use-package composer
+  :ensure t
+  :bind (:map php-mode-map
+              ("C-c c c" . composer-install)
+              ("C-c c u" . composer-update)))
+
+;; --- 6. Autres langages ---
+(use-package dart-mode)
+(use-package lsp-dart :hook (dart-mode . lsp))
+(use-package yaml-mode)
+(use-package sql-indent)
+
+;; --- 7. Tree-sitter (Emacs 29+) ---
+(setq major-mode-remap-alist
+      '((typescript-mode . typescript-ts-mode)
+        (js-mode . js-ts-mode)
+        (bash-mode . bash-ts-mode)
+        (php-mode . php-ts-mode))) ; Utilise le parser moderne pour PHP si dispo
+
+(provide 'init.el)
